d4095e42795509f9faa80d590982dba292e2689a
Pavan Marikanti <marikantipavankumar0@gmail.com>
2026-04-04 00:55:46 +0530

    Added Validations and Security Configurations to avoid csrf error

--- src/main/java/com/example/complaintSystem/dto/ComplaintCreateDto.java
+++ src/main/java/com/example/complaintSystem/dto/ComplaintCreateDto.java
@@ -2,14 +2,16 @@ package com.example.complaintSystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class ComplaintCreateDto {
 
-
     @NotBlank(message = "Title is required")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
 
     @NotBlank(message = "Description is required")
+    @Size(min = 5, max = 1000, message = "Description must be between 5 and 1000 characters")
     private String description;
 
     @NotNull(message = "User Id is required")
